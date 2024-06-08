21eb721abf441cb53b6805b85f010d6c58a1abb2
Aslamzoda Muin <kholovmuin5@gmail.com>
2024-06-08 22:10:19 +0500

    Update Task-4.dart

--- Task-4.dart
+++ Task-4.dart
@@ -1,30 +1,30 @@
 
 
-class Car {
-  String make;
-  String model;
+class Moshin {
+  String soxt;
+  String namud;
 
-  Car({required this.make, required this.model});
+  Moshin({required this.soxt, required this.namud});
 
-  Car.fromMap(Map<String, dynamic> map)
-      : make = map['make'],
-        model = map['model'];
+  Moshin.SSSS(Map<String, dynamic> map)
+      : soxt = map['soxt'],
+        namud = map['namud'];
 }
 
-List<Car> carsFromMaps(List<Map<String, dynamic>> maps) {
-  return maps.map((map) => Car.fromMap(map)).toList();
+List<Moshin> guruhimoshin(List<Map<String, dynamic>> maps) {
+  return maps.map((map) => Moshin.SSSS(map)).toList();
 }
 
 void main() {
-  List<Map<String, dynamic>> carMaps = [
-    {'make': 'Toyota', 'model': 'Corolla'},
-    {'make': 'Honda', 'model': 'Civic'},
-    {'make': 'Ford', 'model': 'Focus'},
+  List<Map<String, dynamic>> moshinMaps = [
+    {'soxt': 'Lada', 'namud': 'priora'},
+    {'soxt': 'Mersedec', 'namud': 'ClS63'},
+    {'soxt': 'Opel', 'namud': 'Astra'},
   ];
 
-  List<Car> cars = carsFromMaps(carMaps);
+  List<Moshin> moshins = guruhimoshin(moshinMaps);
 
-  for (Car car in cars) {
-    print('Make: ${car.make}, Model: ${car.model}');
+  for (Moshin moshin in moshins) {
+    print('Soxt: ${moshin.soxt}, Namud: ${moshin.namud}');
   }
 }
